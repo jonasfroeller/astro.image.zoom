f8cc82b6ae5b59c138809784c1f1416fef0f3ef3
Jonas <publik.froeller.jonas@gmx.at>
2025-01-06 01:23:09 +0100

    chore: update export

--- index.ts
+++ index.ts
@@ -1,9 +1,3 @@
-// Do not write code directly here, instead use the `src` folder!
+import AstroPhotoSwipeLightboxContainer from './src/PhotoSwipeImage.astro';
 
-// What you should do here is re-exports all the things you want your user to access, ex:
-// export { HelloWorld } from "./src/main.ts"
-// export type { HelloWorldResult } from "./src/types.ts"
-
-import AstroImageZoom from './src/PhotoSwipeImage.astro';
-
-export default AstroImageZoom;
+export { AstroPhotoSwipeLightboxContainer };
